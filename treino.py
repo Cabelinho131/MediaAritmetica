a7a429b0d0fa6546b1a6ad360876fb147af8aa1c
Cabelinho131 <161031808+Cabelinho131@users.noreply.github.com>
2025-03-01 19:37:32 -0300

    Add files via upload

---
+++ treino.py
@@ -0,0 +1,12 @@
+
+def aprovação(p1,p2,):
+ media=(p1 + p2 )/2
+ if media >=7:
+  print("aprovado")
+ else:
+   print("reprovado")
+p1 =float(input("informe sua p1:"))
+p2 =float(input("informe sua p2:"))
+aprovação(p1, p2)  
+
+
