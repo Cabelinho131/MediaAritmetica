3400977e548ea449e6237665ade495863ef31dea
Cabelinho131 <161031808+Cabelinho131@users.noreply.github.com>
2025-03-01 21:12:57 -0300

    Update treino.py

--- treino.py
+++ treino.py
@@ -1,12 +1,25 @@
+def aprovação(p1, p2, ac):
+    media_inicial = (p1 * 0.4) + (p2 * 0.4) + (ac * 0.2)
+    if media_inicial >= 7:
+        print(f"Você foi aprovado! Sua média foi: {media_inicial:.2f}")
+        return  
+    print("Sua média foi inferior a 7, precisa fazer a prova substitutiva!")
+    prova_substitutiva = float(input("Informe sua prova substitutiva:"))
+ 
+    if p1 < p2:
+        p1 = prova_substitutiva 
+    else:
+        p2 = prova_substitutiva 
+    
+    media_ajustada = (p1 * 0.4) + (p2 * 0.4) + (ac * 0.2)
+    
+    if media_ajustada >= 7:
+        print(f"Você foi aprovado após a prova substitutiva! Sua média foi: {media_ajustada:.2f}")
+    else:
+        print(f"Reprovado! Sua média foi: {media_ajustada:.2f}")
 
-def aprovação(p1,p2,):
- media=(p1 + p2 )/2
- if media >=7:
-  print("aprovado")
- else:
-   print("reprovado")
-p1 =float(input("informe sua p1:"))
-p2 =float(input("informe sua p2:"))
-aprovação(p1, p2)  
-
+p1 = float(input("Informe sua p1: "))
+p2 = float(input("Informe sua p2: "))
+ac = float(input("Informe sua ac: "))
+aprovação(p1, p2, ac)
 
